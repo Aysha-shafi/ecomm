504a386d3c2097d273dea8885e40c0081032067a
Aysha Shafi <ayshashafi@Ayshas-MacBook-Air.local>
2025-12-03 15:14:46 +0530

    updated

--- server/server.js
+++ server/server.js
@@ -120,6 +120,12 @@ mongoose
   .connect(MONGODB_URI)
   .then(() => {
     console.log('MongoDB connected');
+
+    app.use(express.static(path.join(__dirname, '../client/build')));
+    app.get('*', (req, res) => {
+      res.sendFile(path.join(__dirname, '../client/build', 'index.html'));
+    });
+
     app.listen(PORT, () => console.log('Server started on port', PORT));
   })
   .catch(err => {
